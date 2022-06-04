236c85694798fad3eb08a1e0c3c483b63ab8ee14
PugaKevin <pug20001@byui.edu>
2022-06-04 17:05:18 -0600

    scoreing

---
+++ game/shared/score.py
@@ -0,0 +1,33 @@
+from winreg import REG_OPTION_BACKUP_RESTORE
+
+
+class Score(): 
+
+    def __init__(self):
+        """Intitialies the score as well as changes the font to normal"""
+        self._score1 = 1 
+        self.score_font = pygame.font.SysFont(None,100)
+        
+
+    def get_score(self):
+        """gets score"""
+        return self._score1
+    
+    def update_score(self):
+        self._totalscore = Score().get_score()
+        if robot.get_position().equals(rock.get_position()):
+            self._totalscore -= 30
+        elif rock.get_position().equals(gem.get_position()):
+            self._totalscore +=20 
+        
+        return self._totalscore
+    
+    
+        
+
+        
+
+                
+                
+        
+
