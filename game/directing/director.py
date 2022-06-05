4c8ca81fecd68856f0cc7400a4a9a19bc3cd18b5
DPatter52 <dzpatterson15@gmail.com>
2022-06-04 19:19:34 -0600

    Major update to score functionality

--- game/directing/director.py
+++ game/directing/director.py
@@ -1,4 +1,5 @@
 from game.shared.score import Score
+from tkinter import messagebox
 
 class Director:
     """A person who directs the game. 
@@ -19,6 +20,8 @@ class Director:
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
+        self.score = Score()
+        self.total_score = self.score.get_score()
         
     def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
@@ -33,7 +36,7 @@ class Director:
             self._do_outputs(cast)
         self._video_service.close_window()
 
-    def check_for_end_game(self, score, cast):
+    def check_for_end_game(self, cast):
         """Ends the game based on the score. If the score goes below zero, the 
         player loses. If it gets above 500, the player wins"""
 
@@ -43,12 +46,12 @@ class Director:
             """
         
         banner = cast.get_first_actor("banners")
-        score = Score()
-        total_score = score.update_score()
-        if total_score <= 0:
+         
+        if self.total_score <= 0:
             banner.set_text("Sorry, you lost. Maybe you need some more practice...")
-        elif total_score >= 500:
+        elif self.total_score >= 500:
             banner.set_text("You won! How does it feel to be rich??")
+
 
     def _get_inputs(self, cast):
         """Gets directional input from the keyboard and applies it to the robot.
@@ -66,30 +69,38 @@ class Director:
         Args:
             cast (Cast): The cast of actors.
         """
+        
         banner = cast.get_first_actor("banners")
         robot = cast.get_first_actor("robots")
         rocks= cast.get_actors("rocks")
         gems = cast.get_actors("gems")
         
-        banner.set_text("Score: ")
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
         robot.move_next(max_x, max_y)
         
         for rock in rocks:
             if robot.get_position().equals(rock.get_position()):
+                self.score.update_score("inRocks")
+                self.total_score = self.score.get_score()
                 cast.remove_actor("rocks", rock)
-            for gem in gems:
 
+            for gem in gems:
                 if rock.get_position().equals(gem.get_position()):
                     cast.remove_actor("rocks", rock)
+                    
 
         for gem in gems:
             if robot.get_position().equals(gem.get_position()):
+                self.score.update_score("inGems")
+                self.total_score = self.score.get_score()
                 cast.remove_actor("gems", gem)
                 
 
-
+        
+        banner.set_text(f"Score: {self.total_score}" )
+        
+        
 
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
--- game/shared/score.py
+++ game/shared/score.py
@@ -1,35 +1,45 @@
 from winreg import REG_OPTION_BACKUP_RESTORE
+from game.casting.cast import Cast
+from tkinter import messagebox
+
 
 
 class Score(): 
 
     def __init__(self):
         """Intitialies the score as well as changes the font to normal"""
-        self._score1 = 1 
-        self.score_font = pygame.font.SysFont(None,100)
+        self._score1 = 500 
         
 
     def get_score(self):
         """gets score"""
         return self._score1
     
-    def update_score(self):
-<<<<<<< HEAD
-=======
+    def update_score(self, collision):
+        self._totalscore = self.get_score() 
 
-        robot = cast.get_first_actor("robots")
-        rocks= cast.get_actors("rocks")
-        gems = cast.get_actors("gems")
-
->>>>>>> 852a7504500860d95ce7138dad15b3649dd4207c
-        self._totalscore = Score().get_score()
-        if robot.get_position().equals(rock.get_position()):
+        if collision == "inRocks":
             self._totalscore -= 30
-        elif rock.get_position().equals(gem.get_position()):
-            self._totalscore +=20 
+        elif collision == "inGems":
+            self._totalscore += 20
+        else: 
+            self._totalscore = self._totalscore
+
+
+        #for rock in rocks:
+            #if robot.get_position().equals(rock.get_position()):
+                #self._totalscore -= 30
+                
+
+        #for gem in gems:
+            #if robot.get_position().equals(gem.get_position()):
+                #self._totalscore += 20
+                
+                
+
+        self._score1 = self._totalscore
+        return self._score1
         
-        return self._totalscore
-    
     
         
 
